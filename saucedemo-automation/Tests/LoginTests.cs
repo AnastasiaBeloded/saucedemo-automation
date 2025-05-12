f2d087909f12cf471d549860523f5636e4a6bf4f
AnastasiaBeloded <nastenabeloded@gmail.com>
2025-05-12 22:16:17 +0300

    Fix ChromeDriver headless config for CI

--- saucedemo-automation/Tests/LoginTests.cs
+++ saucedemo-automation/Tests/LoginTests.cs
@@ -8,24 +8,19 @@ namespace SauceDemo.Tests
     [TestFixture]
     public class LoginTests
     {
-        private IWebDriver? driver;
-
+        private IWebDriver driver = null!;
 
         [SetUp]
         public void SetUp()
         {
             var options = new ChromeOptions();
             options.AddArgument("--headless=new");
-            options.AddArgument("--disable-gpu");
             options.AddArgument("--no-sandbox");
+            options.AddArgument("--disable-gpu");
             options.AddArgument("--disable-dev-shm-usage");
-            options.AddArgument("--disable-extensions");
             options.AddArgument("--window-size=1920,1080");
 
             driver = new ChromeDriver(options);
-
-
-            driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl("https://www.saucedemo.com/");
         }
@@ -36,7 +31,7 @@ namespace SauceDemo.Tests
             var loginPage = new LoginPage(driver);
             loginPage.Login("standard_user", "secret_sauce");
 
-            Assert.That(driver.Url.Contains("inventory"), "Login failed – inventory page not reached");
+            Assert.That(driver.Url.Contains("inventory"), "Login failed");
         }
 
         [TearDown]
@@ -48,6 +43,5 @@ namespace SauceDemo.Tests
                 driver.Dispose();
             }
         }
-
     }
 }
