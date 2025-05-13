e0d87e56ed712a0aee3fbfeaa047724623827709
AnastasiaBeloded <nastenabeloded@gmail.com>
2025-05-13 20:30:51 +0300

    Add checkout flow and navigation tests TC21–TC28

---
+++ saucedemo-automation/Pages/CheckoutPage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace saucedemo_automation.Pages
+{
+	public class CheckoutPage
+	{
+		public CheckoutPage()
+		{
+		}
+	}
+}
+
---
+++ saucedemo-automation/Tests/CheckoutTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace saucedemo_automation.Tests
+{
+	public class CheckoutTests
+	{
+		public CheckoutTests()
+		{
+		}
+	}
+}
+
